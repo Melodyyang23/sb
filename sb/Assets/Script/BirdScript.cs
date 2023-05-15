22b2c7a390b262b5fcd35f7c1403cf4b4d4166f4
Melody <2895343953@qq.com>
2023-05-15 23:16:12 +0800

    gameover
    tryagain bottom

--- sb/Assets/Script/BirdScript.cs
+++ sb/Assets/Script/BirdScript.cs
@@ -7,23 +7,28 @@ public class BirdScript : MonoBehaviour
 
     public Rigidbody2D MyRigidBody;
     public float FlapStregth;
+    public LogicScript logic;
+
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space) == true)
+        if (Input.GetKeyDown(KeyCode.Space) == true)
         {
             MyRigidBody.velocity = Vector2.up * FlapStregth;
         }
+    }
 
-      
-
-       
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        logic.gameOver();
     }
 }
--- sb/Assets/Script/LogicScript.cs
+++ sb/Assets/Script/LogicScript.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class LogicScript : MonoBehaviour
 {
     public int score;
     public Text scoretext;
+    public GameObject gameOverScreen;
 
     [ContextMenu("increase score")]
     public void addscore(int ScoreToAdd)
@@ -15,5 +17,14 @@ public class LogicScript : MonoBehaviour
         scoretext.text = score.ToString();
     }
 
+    public void restartGame() 
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void gameOver()
+    {
+        gameOverScreen.SetActive(true);
+    }
 
 }
